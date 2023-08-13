8b8b9d636dc5251e5b9213534a1a9701ac70b76f
Alinho <111993827+AlinhoZz@users.noreply.github.com>
2023-08-13 19:09:20 -0300

    nuvens adicionadas
    adicionei as nuvens ao cenário

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,26 @@
 import pygame
+import random
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, CLOUD    
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
+class Cloud:
+    def __init__(self):
+        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.game_speed = 20
+        self.y = random.randint(50, 100)
+        self.image = CLOUD
+        self.width = self.image.get_width()
+
+    def update(self):
+        self.x -= self.game_speed
+        if self.x < -self.width:
+            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.y = random.randint(100, 200)
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.x, self.y))
 
 class Game:
     def __init__(self):
@@ -18,6 +35,7 @@ class Game:
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.clouds = []
 
     def run(self):
         # Game loop: events - update - draw
@@ -37,13 +55,28 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
+
+        if random.randrange(0, 100) < 1:
+            cloud = Cloud()
+            self.clouds.append(cloud)
         
+        for cloud in self.clouds:
+            cloud.update()
+        self.clouds = [cloud for cloud in self.clouds if cloud.x > -cloud.width]
+
+
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))  # '#FFFFFF'
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        pygame.display.update()
+        pygame.display.flip()
+
+        for cloud in self.clouds:
+            cloud.draw(self.screen)
+
         pygame.display.update()
         pygame.display.flip()
 
@@ -55,3 +88,5 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+        
